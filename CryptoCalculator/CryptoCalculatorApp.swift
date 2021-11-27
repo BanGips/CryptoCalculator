b929cb09e07511d0c2f2e63a6785ca1eba976b62
Sergei Kastyukovets <bangips2@gmail.com>
2021-11-27 12:02:57 +0300

    add project

---
+++ CryptoCalculator/CryptoCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoCalculatorApp.swift
+//  CryptoCalculator
+//
+//  Created by Sergei Kastyukovets on 27.11.21.
+//
+
+import SwiftUI
+
+@main
+struct CryptoCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            TapBarView()
+        }
+    }
+}
---
+++ CryptoCalculator/TapBarView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CryptoCalculator
+//
+//  Created by Sergei Kastyukovets on 27.11.21.
+//
+
+import SwiftUI
+
+struct TapBarView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        TapBarView()
+    }
+}
